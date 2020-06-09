19f356311b9cf363db97bc10887618d674ee79cb
nejdetkadir <nejdetkadir.550@gmail.com>
2020-06-09 12:06:38 +0300

    Add constructor and getters

--- src/Expenses.java
+++ src/Expenses.java
@@ -10,4 +10,32 @@
  */
 public class Expenses {
     
+    int id;
+    String date;
+    String category;
+    float price;
+
+    public Expenses(int id, String date, String category, float price) {
+        this.id = id;
+        this.date = date;
+        this.category = category;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+    
 }
