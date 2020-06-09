3f1ee4236ed1c6da01484fd0f1be9d1199431a48
nejdetkadir <nejdetkadir.550@gmail.com>
2020-06-09 16:23:54 +0300

    Change calculate var type and add months and days table

--- src/Main.java
+++ src/Main.java
@@ -17,14 +17,15 @@ import javax.swing.table.DefaultTableModel;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author nejdetkadirr
  */
 public class Main extends javax.swing.JFrame {
+
     ArrayList<Expenses> data = new ArrayList<>();
     BufferedReader br;
+
     /**
      * Creates new form Main
      */
@@ -34,27 +35,27 @@ public class Main extends javax.swing.JFrame {
         setDataToTable();
         setInfo();
     }
-    
+
     public void setDataToTable() {
         jTable.getColumnModel().getColumn(0).setHeaderValue(data.get(0).getId());
         jTable.getColumnModel().getColumn(1).setHeaderValue(data.get(0).getDate());
         jTable.getColumnModel().getColumn(2).setHeaderValue(data.get(0).getCategory());
         jTable.getColumnModel().getColumn(3).setHeaderValue(data.get(0).getPrice());
-        
+
         for (int i = 1; i < data.size(); i++) {
-            ((DefaultTableModel) jTable.getModel()).addRow(new Object[] {data.get(i).getId(),data.get(i).getDate(),data.get(i).getCategory(),data.get(i).getPrice()});
+            ((DefaultTableModel) jTable.getModel()).addRow(new Object[]{data.get(i).getId(), data.get(i).getDate(), data.get(i).getCategory(), data.get(i).getPrice()});
         }
-        
+
     }
-        
+
     public void readFile() {
         try {
             if (System.getProperty("java.vendor").equals("Ubuntu")) {
-                br = new BufferedReader(new FileReader(new File(System.getProperty("user.dir")+"/src/harcamalar.txt")));
+                br = new BufferedReader(new FileReader(new File(System.getProperty("user.dir") + "/src/harcamalar.txt")));
             } else {
-                br = new BufferedReader(new FileReader(new File(System.getProperty("user.dir")+"\\src\\harcamalar.txt")));
-            }            
-            for(String line; (line = br.readLine()) != null;) {
+                br = new BufferedReader(new FileReader(new File(System.getProperty("user.dir") + "\\src\\harcamalar.txt")));
+            }
+            for (String line; (line = br.readLine()) != null;) {
                 String[] items = line.split(";");
                 data.add(new Expenses(items[0], items[1], items[2], items[3]));
             }
@@ -64,38 +65,70 @@ public class Main extends javax.swing.JFrame {
             JOptionPane.showMessageDialog(this, ex.getLocalizedMessage());
         }
     }
-    
+
     public void setInfo() {
         //Calculate total price
-        float totalPrice = 0;
+        double totalPrice = 0;
         for (int i = 1; i < data.size(); i++) {
-            totalPrice += Float.valueOf(data.get(i).getPrice());
+            double price = Double.valueOf(data.get(i).getPrice());
+            System.out.println(price);
         }
         jLabelTotalPrice.setText("Toplam Harcama Tutarı : " + totalPrice);
-        
-        //Calculate total prices of categories
-        HashMap<String, Float> categories = new HashMap<>();
-        for (Expenses e: data) {
-            if (!e.getCategory().equals("KATEGORİ")) {
-                if (categories.containsKey(e.getCategory())) {
-                    float price = categories.get(e.getCategory());
-                    categories.put(e.getCategory(), categories.get(e.getCategory()) + price);
-                    System.out.println("Burda");
 
+        //Calculate total prices of categories
+        HashMap<String, Double> categories = new HashMap<>();
+        HashMap<String, Double> days = new HashMap<>();
+        HashMap<String, Double> months = new HashMap<>();
+        for (Expenses e : data) {
+            //Calculate total prices of every categories
+            if (!e.getCategory().equals("KATEGORİ")) {
+                double price = Double.valueOf(e.getPrice());
+                if (categories.containsKey(e.getCategory())) {                    
+                    categories.put(e.getCategory(), categories.get(e.getCategory()) + price);
                 } else {
-                    categories.put(e.getCategory(), Float.valueOf(e.getPrice()));                    
-                }                
+                    categories.put(e.getCategory(), Double.valueOf(e.getPrice()));
+                }
+            }
+            //Calculate total prices of days
+            if (!e.getCategory().equals("KATEGORİ")) {
+                if (days.containsKey(e.getDay())) {
+                    double price = Double.valueOf(e.getPrice());
+                    days.put(e.getDay(), days.get(e.getDay()) + price);
+                } else {
+                    days.put(e.getDay(), Double.valueOf(e.getPrice()));
+                }
+            }
+            //Calculate total prices of months
+            if (!e.getCategory().equals("KATEGORİ")) {
+                if (months.containsKey(e.getMonth())) {
+                    double price = Double.valueOf(e.getPrice());
+                    months.put(e.getMonth(), months.get(e.getMonth()) + price);
+                } else {
+                    months.put(e.getMonth(), Double.valueOf(e.getPrice()));
+                }
             }
         }
-        
-        for(Map.Entry<String, Float> entry : categories.entrySet()) {
+
+        for (Map.Entry<String, Double> entry : categories.entrySet()) {
             String key = entry.getKey();
-            float value = entry.getValue();
-            ((DefaultTableModel) jTable1.getModel()).addRow(new Object[] {key, value}); 
+            double value = entry.getValue();
+            ((DefaultTableModel) jTable1.getModel()).addRow(new Object[]{key, value});
+        }
+
+        for (Map.Entry<String, Double> entry : days.entrySet()) {
+            String key = entry.getKey();
+            double value = entry.getValue();
+            ((DefaultTableModel) jTable2.getModel()).addRow(new Object[]{key, value});
         }
         
+        for (Map.Entry<String, Double> entry : months.entrySet()) {
+            String key = entry.getKey();
+            double value = entry.getValue();
+            ((DefaultTableModel) jTable3.getModel()).addRow(new Object[]{key, value});
+        }
+
     }
-    
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -110,7 +143,13 @@ public class Main extends javax.swing.JFrame {
         jLabelTotalPrice = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jLabelTotalPrice1 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable2 = new javax.swing.JTable();
+        jLabel2 = new javax.swing.JLabel();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        jTable3 = new javax.swing.JTable();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
@@ -165,8 +204,58 @@ public class Main extends javax.swing.JFrame {
             jTable1.getColumnModel().getColumn(1).setResizable(false);
         }
 
-        jLabelTotalPrice1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabelTotalPrice1.setText("Kategorilerin Harcama Tutarı");
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Kategorilerin Harcama Tutarı");
+
+        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Günler", "Toplam"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane2.setViewportView(jTable2);
+        if (jTable2.getColumnModel().getColumnCount() > 0) {
+            jTable2.getColumnModel().getColumn(0).setResizable(false);
+            jTable2.getColumnModel().getColumn(1).setResizable(false);
+        }
+
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText("Günlerin Harcama Tutarı");
+
+        jTable3.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Aylar", "Toplam"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        jScrollPane3.setViewportView(jTable3);
+        if (jTable3.getColumnModel().getColumnCount() > 0) {
+            jTable3.getColumnModel().getColumn(0).setResizable(false);
+            jTable3.getColumnModel().getColumn(1).setResizable(false);
+        }
+
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("Ayların Harcama Tutarı");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -177,10 +266,18 @@ public class Main extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jScrollPane)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabelTotalPrice, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 227, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabelTotalPrice, javax.swing.GroupLayout.PREFERRED_SIZE, 300, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 15, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jLabelTotalPrice1, javax.swing.GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 200, Short.MAX_VALUE)
                             .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))))
                 .addContainerGap())
         );
@@ -189,12 +286,24 @@ public class Main extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabelTotalPrice)
-                    .addComponent(jLabelTotalPrice1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 257, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabelTotalPrice)
+                            .addComponent(jLabel1))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(0, 95, Short.MAX_VALUE)
+                                .addComponent(jLabel2)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
 
@@ -237,11 +346,17 @@ public class Main extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabelTotalPrice;
-    private javax.swing.JLabel jLabelTotalPrice1;
     private javax.swing.JScrollPane jScrollPane;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JTable jTable;
     private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTable2;
+    private javax.swing.JTable jTable3;
     // End of variables declaration//GEN-END:variables
 }
