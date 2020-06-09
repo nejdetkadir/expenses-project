65bd70e4191d95b7cb4c85ac6d1a523503b7c2d0
nejdetkadir <nejdetkadir.550@gmail.com>
2020-06-09 16:04:59 +0300

    Add func for name day of time

--- src/Expenses.java
+++ src/Expenses.java
@@ -1,3 +1,10 @@
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -36,6 +43,43 @@ public class Expenses {
 
     public String getPrice() {
         return price;
+    }
+    
+    public Date getSimpleDateFormat() {
+        try {
+            return new SimpleDateFormat("d.M.yyyy").parse(getDate());
+        } catch (ParseException ex) {
+            Logger.getLogger(Expenses.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    public String getDay() {
+        String nameofday = "";
+        switch (getSimpleDateFormat().toString().split(" ")[0]) {
+            case "Thu":
+                nameofday = "Perşembe";
+                break;
+            case "Tue":
+                nameofday = "Salı";
+                break;
+            case "Sat":
+                nameofday = "Cumartesi";
+                break;    
+            case "Wed":
+                nameofday = "Çarşamba";
+                break;
+            case "Fri":
+                nameofday = "Cuma";
+                break;
+            case "Sun":
+                nameofday = "Pazar";
+                break;
+            case "Mon":
+                nameofday = "Pazartesi";
+                break;            
+        }        
+        return nameofday;
     }
     
 }
